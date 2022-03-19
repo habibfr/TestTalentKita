5deb65ed5168243c2ba518b794e687a6b6dccb78
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-19 12:39:01 +0700

    body

--- talentkita/src/component/Body.jsx
+++ talentkita/src/component/Body.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./content/About";
 import Section3 from "./content/Section3";
+import Section4 from "./content/Section4";
 import Services from "./content/Services";
 
 const Body = () => {
@@ -9,6 +10,7 @@ const Body = () => {
       <About />
       <Services/>
       <Section3/>
+      <Section4/>
     </div>
   );
 };
