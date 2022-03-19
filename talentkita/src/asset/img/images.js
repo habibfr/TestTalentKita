8747d4e7b00085a34e2e76456a444a00a27196a7
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-19 11:38:32 +0700

    image

---
+++ talentkita/src/asset/img/images.js
@@ -0,0 +1,14 @@
+import scene from "./Scenes03.svg";
+import iklan1 from "./asus.png";
+import iklan2 from "./amazon.png";
+import iklan3 from "./adidas.png";
+import comp from "./christopher-gower-m_HRfLhgABo-unsplash.jpg";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  scene,
+  iklan1,
+  iklan2,
+  iklan3,
+  comp,
+};
