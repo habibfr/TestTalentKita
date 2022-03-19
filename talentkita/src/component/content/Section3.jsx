285f5bd302d2fdff0786adf755d6c23b24c3b3c5
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-19 11:39:59 +0700

    section3

---
+++ talentkita/src/component/content/Section3.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Section3 = () => {
+  return (
+    <div className="w-full bg-blue-100">
+      <div className="container py-6 max-w-4xl mx-auto">
+        <div className="flex justify-center items-center py-6">
+          <div className="w-full font-bold text-3xl">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+          <div className="w-full text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standarLorem Ipsum is
+            simply dummy text of the printing and typesetting industry.
+          </div>
+        </div>
+        <div className="flex gap-8 py-6">
+          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
+            <div className="p-8">
+              <div className="font-bold mb-2 text-xl">Invoicing</div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+          </div>
+          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
+            <div className="p-8">
+              <div className="font-bold mb-2 text-xl">Invoicing</div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+          </div>
+          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
+            <div className="p-8">
+              <div className="font-bold mb-2 text-xl">Invoicing</div>
+              <div>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
