4a6f8c713fd60b7c373eaa44fe2f51b42376d86a
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-20 10:21:39 +0700

    make dinamic

--- talentkita/src/component/content/Section3.jsx
+++ talentkita/src/component/content/Section3.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 
+const dataCard = [
+  {
+    title: "Invoicing",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+  },
+  {
+    title: "Invoicing",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+  },
+  {
+    title: "Invoicing",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting",
+  },
+];
+
 const Section3 = () => {
+  const cards = dataCard.map((card) => {
+    return (
+      <div key={card.title} className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
+        <div className="px-8 pb-8">
+        <div className="font-bold text-4xl text-orange-600">*</div>
+          <div className="font-bold mb-2 text-xl">{card.title}</div>
+          <div className="text-sm">
+            {card.desc}
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div className="w-full bg-blue-100">
+    <div className="w-full bg-white">
       <div className="container py-6 max-w-4xl mx-auto">
         <div className="flex justify-center items-center py-6">
           <div className="w-full font-bold text-3xl mr-10">
@@ -16,33 +45,7 @@ const Section3 = () => {
           </div>
         </div>
         <div className="flex gap-8 py-6">
-          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
-            <div className="p-8">
-              <div className="font-bold mb-2 text-xl">Invoicing</div>
-              <div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </div>
-            </div>
-          </div>
-          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
-            <div className="p-8">
-              <div className="font-bold mb-2 text-xl">Invoicing</div>
-              <div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </div>
-            </div>
-          </div>
-          <div className="bg-white max-w-xs rounded-xl shadow-lg pt-6">
-            <div className="p-8">
-              <div className="font-bold mb-2 text-xl">Invoicing</div>
-              <div>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
-              </div>
-            </div>
-          </div>
+          {cards}
         </div>
       </div>
     </div>
