7e0211cb8ec38fa831c439a4737561fa5bd7773c
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-20 10:19:33 +0700

    card

--- talentkita/src/component/UI/Card.jsx
+++ talentkita/src/component/UI/Card.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Card = (props) => {
   return (
     <>
-      <div className="max-w-xs">
+      <div className={"max-w-xs " + props.className} >
         <img
-          className="w-full rounded-xl "
+          className={"w-full " + props.classImg}
           src={props.image}
           alt="Sunset in the mountains"
         />
-        <div class=" py-4">
+        <div className={"py-4 " + props.classDesc}>
           <div className="font-bold text-xl mb-2">{props.title}</div>
-          <p className="text-gray-700 text-base">{props.desc}</p>
+          <p className="text-gray-700 text-sm">{props.desc}</p>
         </div>
 
         <div className="">
-          <span className="text-base text-orange-600 transition-all hover:text-orange-800 cursor-pointer"> Learn More </span>
+          <span className={"text-base text-orange-600 transition-all hover:text-orange-800 cursor-pointer " + props.classLearn}> Learn More </span>
         </div>
       </div>
     </>
