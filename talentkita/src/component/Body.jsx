8d61d7e8e38f94b8b8b771d6e5d522e20ecb8af1
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-20 10:20:28 +0700

    finish

--- talentkita/src/component/Body.jsx
+++ talentkita/src/component/Body.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import About from "./content/About";
+import Blog from "./content/Blog";
 import Section3 from "./content/Section3";
 import Section4 from "./content/Section4";
+import Section5 from "./content/Section5";
 import Services from "./content/Services";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
-    <div className="mb-80">
+    <div className="">
       <About />
       <Services/>
       <Section3/>
       <Section4/>
+      <Section5/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 };
