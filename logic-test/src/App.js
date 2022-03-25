5befbb2dac7172a463b98cb5f7efb8b194af5122
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-25 20:35:32 +0700

    add segitiga

--- logic-test/src/App.js
+++ logic-test/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HitungBungaDinamis from "./components/HitungBungaDinamis";
 import HitungBungaStatic from "./components/HitungBungaStatic";
+import Segitiga from "./components/Segitiga";
 
 const App = () => {
   return (
     <div>
       <HitungBungaStatic/>
       <HitungBungaDinamis />
+      <Segitiga/>
     </div>
   );
 };
