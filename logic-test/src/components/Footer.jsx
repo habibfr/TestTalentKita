866f204a5cd28e07b4b3fff08da4fdec588b5d7c
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-03-26 13:39:18 +0700

    footer

---
+++ logic-test/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-purple-900">
+      <div className="px-4 py-3 text-center font-medium text-lg text-slate-300">
+        <span>Made by <a className="transition-all duration-500 ease-in hover:text-cyan-400" href="https://habibfr.vercel.app/">Habibfr.</a></span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
